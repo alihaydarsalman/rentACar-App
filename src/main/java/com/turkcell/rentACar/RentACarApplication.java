4922dc3f9e6fb1a1f70b4011a0704f9c0e00934d
Alihaydar SALMAN <1030522871@erciyes.edu.tr>
2022-04-29 11:05:48 +0300

    app config updated to deploy on heroku

--- src/main/java/com/turkcell/rentACar/RentACarApplication.java
+++ src/main/java/com/turkcell/rentACar/RentACarApplication.java
@@ -28,6 +28,4 @@ public class RentACarApplication {
 		return new ModelMapper();
 	}
 
-
-
 }
--- src/main/java/com/turkcell/rentACar/dataAccess/BrandDao.java
+++ src/main/java/com/turkcell/rentACar/dataAccess/BrandDao.java
@@ -8,6 +8,4 @@ import org.springframework.stereotype.Repository;
 public interface BrandDao extends JpaRepository<Brand,Integer> {
     boolean existsByBrandName(String brandName);
     boolean existsByBrandId(int brandId);
-
-
 }
